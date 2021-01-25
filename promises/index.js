0d5ab45e1c4e0173e8c104b8479fbcf42ecdaa63
Breens-Thomas <mbakabreens@gmail.com>
2021-01-25 16:09:28 +0300

    Read the cat Api and print out all of the cat names in the catNames variable

--- promises/index.js
+++ promises/index.js
@@ -1,4 +1,7 @@
-//Read the countries API using fetch and print the name of country, capital, languages, population and area.
+/**
+ * Exercise1: Read the countries API using fetch and print the name of country, 
+ * capital, languages, population and area.
+ */
 const countriesAPI = 'https://restcountries.eu/rest/v2/all'
 const countryData = async () => {
     const response =  await fetch(countriesAPI);
@@ -11,3 +14,17 @@ const countryData = async () => {
     });
 }
 countryData();
+
+
+//Exercise2: Print out all the cat names in to catNames variable
+const catsAPI = 'https://api.thecatapi.com/v1/breeds';
+const catsData = async () => {
+    var response = await fetch(catsAPI);
+    var data = await response.json();
+    var catNames;
+    data.forEach(Catname => {
+        catNames = Catname.name;
+        console.log(catNames);
+    });
+}
+catsData();
